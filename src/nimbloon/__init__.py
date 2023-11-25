1ee9591845b7bd4fb49a5b393d41ba26f91b0215
fcomitani <federico.comitani@gmail.com>
2023-11-25 11:00:07 -0500

    fix version

--- src/nimbloon/__init__.py
+++ src/nimbloon/__init__.py
@@ -3,6 +3,16 @@ Nimble RACCOON
 F. Comitani     @2023-2024
 """
 
+import toml
+from pathlib import Path
+
 from .raccoon import Raccoon
 
-__version__ = "0.1.0"
+def get_version():
+    pyproject_toml_path = Path(__file__).resolve()\
+        .parent.parent.parent / "pyproject.toml"
+    pyproject_toml = toml.load(pyproject_toml_path)
+    
+    return pyproject_toml["project"]["version"]
+
+__version__ = get_version()
