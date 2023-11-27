4b9da91c6bf5b61b89c6c6eb9610de822ebe3616
fcomitani <federico.comitani@gmail.com>
2023-11-27 18:42:44 -0500

    remove version

--- src/nimbloon/__init__.py
+++ src/nimbloon/__init__.py
@@ -1,18 +1,1 @@
-"""
-Nimble RACCOON
-F. Comitani     @2023-2024
-"""
-
-import toml
-from pathlib import Path
-
 from .raccoon import Raccoon
-
-def get_version():
-    pyproject_toml_path = Path(__file__).resolve()\
-        .parent.parent / "pyproject.toml"
-    pyproject_toml = toml.load(pyproject_toml_path)
-    
-    return pyproject_toml["project"]["version"]
-
-__version__ = get_version()
